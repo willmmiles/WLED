7676a2f9b8e1a386eeae4bce8824f407519451cf
Will Miles <will@willmiles.net>
2025-01-15 02:40:47 +0000

    load_usermods debug

--- pio-scripts/load_usermods.py
+++ pio-scripts/load_usermods.py
@@ -37,24 +37,27 @@ if usermods:
 # Save the old value
 cpl = env.ConfigureProjectLibBuilder
 # Our new wrapper
-def cpl_wrapper(env):
-  # Update usermod properties
-  lib_builders = env.GetLibBuilders()  
+def cpl_wrapper(xenv):
+  # Update usermod properties  
+  lib_builders = xenv.GetLibBuilders()  
+  wled_dir = xenv["PROJECT_SRC_DIR"]
   um_deps = [dep for dep in lib_builders if usermod_dir in Path(dep.src_dir).parents]
   other_deps = [dep for dep in lib_builders if usermod_dir not in Path(dep.src_dir).parents]
+  print(xenv.Dump())
   for um in um_deps:
     # Add include paths for all non-usermod dependencies
     for dep in other_deps:
         for dir in dep.get_include_dirs():
             um.env.PrependUnique(CPPPATH=dir)
-    # Add the wled folder to the include path    
-    um.env.PrependUnique(CPPPATH=env["PROJECT_SRC_DIR"])
+    # Add the wled folder to the include path
+    print(f"{str(um)}: adding {wled_dir}")
+    um.env.PrependUnique(CPPPATH=wled_dir)
     # Make sure we link directly, not through an archive
     # Archives drop the .dtor table section we need
     build = um._manifest.get("build", {})
     build["libArchive"] = False
     um._manifest["build"] = build
-  return cpl.clone(env)()
+  return cpl.clone(xenv)()
 
 
 # Replace the old one with ours
