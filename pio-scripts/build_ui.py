c5baf7e3cd6271544c96f677c9aaa6b94e343100
Will Miles <will@willmiles.net>
2025-12-21 18:05:09 -0500

    build_ui: Build only when needed
    Use SCons dependencies to only run npm if we need it.

--- pio-scripts/build_ui.py
+++ pio-scripts/build_ui.py
@@ -1,4 +1,5 @@
 Import("env")
+from pathlib import Path   # For OS-agnostic path manipulation
 import shutil
 
 node_ex = shutil.which("node")
@@ -7,7 +8,9 @@ if node_ex is None:
     print('\x1b[0;31;43m' + 'Node.js is not installed or missing from PATH html css js will not be processed check https://kno.wled.ge/advanced/compiling-wled/' + '\x1b[0m')
     exitCode = env.Execute("null")
     exit(exitCode)
-else:
+
+
+def build_ui(target, source, env):
     # Install the necessary node packages for the pre-build asset bundling script
     print('\x1b[6;33;42m' + 'Installing node packages' + '\x1b[0m')
     env.Execute("npm ci")
@@ -19,3 +22,31 @@ else:
     if (exitCode):
       print('\x1b[0;31;43m' + 'npm run build fails check https://kno.wled.ge/advanced/compiling-wled/' + '\x1b[0m')
       exit(exitCode)
+
+ui_srcdir = Path(env["PROJECT_DIR"]).resolve() / "wled00" / "data"
+
+build_ui_cmd = env.Command(
+   target=env.File('$PROJECT_DIR/wled00/html_ui.h'),   # A representative file
+   source=[
+      env.File('$PROJECT_DIR/package-lock.json'),
+      env.File('$PROJECT_DIR/package.json'),
+      env.File('$PROJECT_DIR/tools/cdata.js'),
+      *[env.File(str(p)) for p in ui_srcdir.rglob("*") if p.is_file()]
+   ],
+   action=build_ui)
+
+# Ensure UI gets built before any cpp files by hooking the Object constructor
+def _wrap_object_method(name):
+    if not hasattr(env, name): return
+    orig = getattr(env, name)
+    def wrapped(*args, **kwargs):
+        nodes = orig(*args, **kwargs)
+        env.Requires(nodes, build_ui_cmd)
+        return nodes
+    setattr(env, name, wrapped)
+
+for m in ("Object", "StaticObject", "SharedObject"):
+    _wrap_object_method(m)
+
+# Also make an an explicit dependency of the final build target, so it will always be built the first time
+env.Depends("$BUILD_DIR/$PROGNAME$PROGSUFFIX", build_ui_cmd)
